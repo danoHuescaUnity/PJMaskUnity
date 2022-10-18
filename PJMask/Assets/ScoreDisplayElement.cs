8c186526fe82ca92f0881f8846b17ed61f8eb3de
luishNvizzio <dano.huesca@gmail.com>
2022-10-18 16:47:27 -0500

    added score displayElement

---
+++ PJMask/Assets/ScoreDisplayElement.cs
@@ -0,0 +1,24 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplayElement : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Text scoreText = null;
+    [SerializeField]
+    private string initializeText = null;
+
+    void Start()
+    {
+        Player local = PhotonNetwork.LocalPlayer;
+
+        if (local.CustomProperties.TryGetValue("Score", out object score))
+        {
+            scoreText.text = initializeText + " " + score.ToString();
+        }
+    }
+}
